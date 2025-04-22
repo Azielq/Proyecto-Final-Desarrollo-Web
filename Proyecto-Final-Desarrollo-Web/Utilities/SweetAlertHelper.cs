0eea215b2b5b4a451c744849d2aaf16bcb8ad368
IgnisMT <josejoss@ymail.com>
2025-04-22 08:36:35 -0600

    • Actualice el Model de clientes con validaciones y lógica • Actualice los controladores de clientes y categoría con todo lo necesario para que funcionen • Agregué un Helper y un parcial para sweetalert2, además añadí este último al Layout • Agregué: • Categorías (esta con modales) + Index.cshtml • Clientes + ComprasCliente.cshtml + Create.cshtml + Delete.cshtml + Details.cshtml + Edit.cshtml + Index.cshtml

---
+++ Proyecto-Final-Desarrollo-Web/Utilities/SweetAlertHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Final_Desarrollo_Web.Helpers
+{
+    public class SweetAlertHelper
+    {
+        public static void ShowSweetAlert(Controller controller, string title, string message, SweetAlertMessageType messageType)
+        {
+            controller.TempData["SweetAlertTitle"] = title;
+            controller.TempData["SweetAlertMessage"] = message;
+            controller.TempData["SweetAlertType"] = messageType.ToString().ToLower();
+        }
+
+        public static void ShowSweetAlertWithRedirect(Controller controller, string title, string message, SweetAlertMessageType messageType, string redirectUrl)
+        {
+            controller.TempData["SweetAlertTitle"] = title;
+            controller.TempData["SweetAlertMessage"] = message;
+            controller.TempData["SweetAlertType"] = messageType.ToString().ToLower();
+            controller.TempData["RedirectUrl"] = redirectUrl;
+        }
+
+        public static void ShowToast(Controller controller, string message, SweetAlertMessageType messageType)
+        {
+            controller.TempData["SweetAlertMessage"] = message;
+            controller.TempData["SweetAlertType"] = messageType.ToString().ToLower();
+            controller.TempData["SweetAlertIsToast"] = true;
+        }
+    }
+
+    public enum SweetAlertMessageType
+    {
+        Success,
+        Error,
+        Warning,
+        Info,
+        Question
+    }
+}
